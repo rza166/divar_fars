fddaa270a0d972d4895398ca19d559395e33766c
unknown <esi191@yahoo.com>
2019-10-01 20:29:53 +0330

    '21'

--- public/js/main.js
+++ public/js/main.js
@@ -24,8 +24,32 @@ function  sho() {
 
 }
 function showMenu(classShow , classI) {
-  $('.li_4').css('display','none');
-  $('.' + classShow ).css('display','block');
-  $('.' + classI ).css('display','inline-block')
-  $('.e40-1').css('display','none');
+  var cmd = $('.' + classShow ).attr('data-show');
+  if (cmd=='show') {
+    $('.li_4').css('display','none');
+    $('.' + classShow ).css('display','block');
+    $('.' + classI ).css('display','inline-block')
+    $('.e40-1').css('display','none');
+    $('.li_4').css({"font-size":"22px","padding-top": "2px" , "border-bottom":"2px solid rgb(201, 201, 201)"});
+    $('.' + classShow ).attr('data-show','hide');
+  }
+  else{
+    $('.li_4').css('display','block');
+    // $('.' + classShow ).css('display','none');
+    $('.' + classI ).css('display','none')
+    // $('.e40-1').css('display','inline-block');
+    $( ".li1" ).hover ( function() {$('.e40-1-1').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $( ".li2" ).hover ( function() {$('.e40-1-2').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $( ".li3" ).hover ( function() {$('.e40-1-3').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $( ".li4" ).hover ( function() {$('.e40-1-4').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $( ".li5" ).hover ( function() {$('.e40-1-5').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $( ".li6" ).hover ( function() {$('.e40-1-6').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $( ".li7" ).hover ( function() {$('.e40-1-7').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $( ".li8" ).hover ( function() {$('.e40-1-8').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $( ".li9" ).hover ( function() {$('.e40-1-9').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $( ".li10" ).hover ( function() {$('.e40-1-10').css('display','inline-block');},function() {$('.e40-1').css('display','none');} );
+    $('.li_4').css({"font-size":"18px","padding-top": "2px" , "border-bottom":"2px solid rgb(201, 201, 201)"});
+    $('.' + classShow ).attr('data-show','show');
+  }
+
 }
