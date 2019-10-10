6c888009e3e527378cd7fa2e1d626decd4f84101
unknown <esi191@yahoo.com>
2019-10-10 11:54:49 +0330

    123

--- resources/views/agahi/layoutAgahi.blade.php
+++ resources/views/agahi/layoutAgahi.blade.php
@@ -6,18 +6,17 @@
 </div>
 </a>
 <div class="a2">
-  <div class="container">
-  <form >
-    <div class="form-group ">
-      <label for="sel1">شهر:</label>
-      <select class="form-control" id="sel1" name="sellist1">
-        <option>ارسنجان</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-      </select>
-    </div>
-  </form>
-</div>
+  <div class=" a3">
+    <label class="labela1" for="">شهر</label>
+    <input type="text" class=" e22 e24 e26" placeholder="انتخاب شهر" id="usr2" name="username">
+  <div class=" e21 ">
+    <i class='fas fa-caret-down e25' style='font-size:14px'></i>
+  </div >
+  <label class="labela2" for="">نقشه</label>
+
+  </div>
+  <div class="a4">
+    ss
+  </div>
 
 </div>
